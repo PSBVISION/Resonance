852334b3374dc41c95c2f2a193e85e16f7b24ff3
psbvision <punitsinghbisht1529@gmail.com>
2025-08-29 23:46:23 +0530

    feat: setup convex

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@workspace/ui/components/button"
-import { Input } from "@workspace/ui/components/input"
+import { useQuery } from "convex/react"
 export default function Page() {
+  const users = useQuery(api.users.getMany);
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-        <Input></Input>
-      </div>
+      <p>apps/web</p>
     </div>
   )
 }
