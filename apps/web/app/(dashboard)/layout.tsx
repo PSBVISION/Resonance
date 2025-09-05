1e60f8453de18e0a76c80447c491d5191ed8b023
psbvision <punitsinghbisht1529@gmail.com>
2025-09-05 23:25:36 +0530

    feat: add sidebar

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,14 +1,8 @@
-import { AuthGuard } from '@/modules/auth/ui/components/auth-guard'
-import { OrganizationGuard } from '@/modules/auth/ui/components/organization-guard'
 
-const Layout = ({children}:{children:React.ReactNode}) => {
-  return (
-    <AuthGuard>
-      <OrganizationGuard>
-      {children}
-      </OrganizationGuard>
-    </AuthGuard>
-  )
-}
+import { DashboardLayout } from "@/modules/dashboard/ui/layouts/dashboard-layout";
 
-export default Layout
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return <DashboardLayout>{children}</DashboardLayout>;
+};
+
+export default Layout;
